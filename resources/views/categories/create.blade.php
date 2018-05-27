0caf732934c27f5764275fe52e82105800fc263b
Tjoosten <Topairy@gmail.com>
2018-05-27 20:16:01 +0200

    Complete form
    Complete the form for the category insert

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -8,19 +8,33 @@
 
         <div class="col-9"> {{-- Content --}}
             <div class="card card-body border-0 box-shadow">
-                <h6 class="border-bottom border-gray pb-2 mb-3">Create new privacy concern category</h6>
+                <h6 class="border-bottom border-gray pb-2 mb-3">Create new category</h6>
 
                 <form method="POST" action="">
                     @csrf {{-- Form field protection --}}
 
                     <div class="form-group row">
                         <label for="name" class="col-3 col-form-label">Name <span class="input-required">*</span></label>
+
+                        <div class="col-9">
+                            <input type="text" id="name" class="form-control @error('name', 'is-invalid')" placeholder="Name for the category" @input('name')>
+                            @error('name', '<div class="invalid-feedback">:message</div>')
+                        </div>
                     </div>
 
                     <div class="form-group row">
                         <label for="description" class="col-3 col-form-label"> Description <span class="input-required">*</span></label>
 
                         <div class="col-9">
+                            <textarea @input('dscription') aria-describedby="descHelpBlock" id="description" class="form-control @error('description', 'is-invalid')" placeholder="Category description" rows="7"></textarea>
+                        
+                            @if ($errors->has('description'))
+                                @error ('description', '<div class="invalid-feedback">:message</div>') {{-- Validation error partial --}}
+                            @else {{-- No validation errors are found --}}
+                                <small id="descHelpBlock" class="form-text text-muted">
+                                    <span class="input-required">*</span> Keep the description short as possible.
+                                </small>
+                            @endif
                         </div>
                     </div>
 
@@ -31,7 +45,7 @@
                             </button>
 
                             <button type="reset" class="btn btn-outline-danger">
-                                <i class="fas fa-fw fa-undo"> Reset
+                                <i class="fas fa-fw fa-undo"></i> Reset
                             </button>
                         </div>
                     </div>
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -8,12 +8,13 @@
 
         <div class="col-9"> {{-- Content --}}
             <div class="card card-body border-0 box-shadow">
-                <h6 class="border-bottom border-gray pb-2 mb-3">Privacy categories overview</h6>
+                <h6 class="border-bottom border-gray pb-2 mb-3">Application categories overview</h6>
 
                 <table class="table table-sm">
                     <thead>
                         <tr>
                             <th class="no-border">Name:</th>
+                            <th class="no-border">Module:</th>
                             <th class="no-border">Description:</th>
                         </tr>
                     </thead>
